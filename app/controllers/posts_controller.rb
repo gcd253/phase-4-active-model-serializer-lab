66f0eb225f2fbe38ea70d448d32d104a518fd767
Griffin Dooley <dooleygriffin@gmail.com>
2022-07-25 07:37:58 -0400

    commit tree 2157bc917781ce1fe9a727a9b15cb3ddb8d99e1b

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,12 @@ class PostsController < ApplicationController
 
   def index
     posts = Post.all
-    render json: posts
+    render json: posts, include: ['posts']
   end
 
   def show
     post = Post.find(params[:id])
-    render json: post
+    render json: post, include: ['posts']
   end
 
   private
